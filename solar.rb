282705a384c78c22b0c0af0ffe79dfdbfc410b2c
Justin Cheung <justinwkcheung@github.com>
2017-01-20 10:11:11 -0500

    Complete bonus exercises

--- solar.rb
+++ solar.rb
@@ -3,21 +3,21 @@ class System
   @@all_systems = []
 
 
-  attr_reader :bodies, :total_mass, :masses
+  attr_reader :bodies, :masses
   def initialize
     @bodies = []
-    @total_mass = 0
     @@all_systems.push(self)
   end
 
-  def self.total_mass
-    total_mass = 0
+  def self.total_mass_gal
+    total_mass_gal = 0
 
     @@all_systems.each do |system|
-      total_mass += system.total_mass
+      total_mass_gal += system.total_mass
+      puts "Systems = #{@@all_systems}"
     end
 
-    total_mass
+    total_mass_gal
   end
 
 
@@ -27,22 +27,23 @@ class System
         return "This body already exists!"
       end
     end
+    puts "adding body #{body.name} to system"
     @bodies << body
-    return "the list of bodies is #{@bodies}"
+    @bodies
   end
 
 
   def total_mass
+    total_mass = 0
     @bodies.each do |body|
 
-      @total_mass += body.mass
+      total_mass += body.mass
     end
-    @total_mass
+    total_mass
   end
 
 end
 
-system = System.new
 
 
 class Body
@@ -109,6 +110,9 @@ class Star < Body
 end
 
 class Moon < Body
+
+  @@moon_count = 0
+
   attr_reader :month, :planet
   def initialize(name, mass, month, planet)
     super(name, mass)
@@ -126,9 +130,12 @@ class Moon < Body
         @@moon_count += 1
       end
     end
-    return @@star_count
+    return @@moon_count
   end
 end
+
+
+# DO THESE THINGS -----------------------------
 
 earth = Planet.new("earth", 50, 24, 365)
 mars = Planet.new("mars", 80, 30, 400)
@@ -136,13 +143,17 @@ bars = Planet.new("mars", 40, 30, 200)
 sun = Star.new("sun", 90, "g-star")
 titan = Moon.new("titan", 30, 20, "earth")
 
+system = System.new
 puts system.add(earth)
 puts system.add(mars)
 puts system.add(mars)
 puts system.add(sun)
 puts system.add(titan)
 puts "System total mass is #{system.total_mass}"
-alpha = System.new
-puts "Alpha's total mass is #{alpha.total_mass}"
+
+# alpha = System.new
+# puts alpha.add(earth)
+# puts "Alpha's total mass is #{alpha.total_mass}"
 puts Planet.all(system)
-puts "System galatic mass is #{System.total_mass}"
+puts "System galatic mass is #{System.total_mass_gal}"
+puts "Number of moons in system is #{Moon.all(system)}"
